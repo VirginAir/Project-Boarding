9e771e2a1b1612ea17d6e57fcf1396f5031f0e35
Matthew <Matthew@matthews-mbp.home>
2014-09-29 22:24:14 +0100

    Updating entire hub

--- src/projectboarding/ProjectBoarding.java
+++ src/projectboarding/ProjectBoarding.java
@@ -10,7 +10,6 @@ public class ProjectBoarding {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
         // Create and run the boarding
         BoardingController controller = new BoardingController();
         controller.startBoarding();
